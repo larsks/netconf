4ee5cdfaf21bc192cd8b15a5520c41ae257900d3
Lars Kellogg-Stedman <lars@oddbit.com>
2012-03-09 23:46:45 -0500

    prevent "config" vlan file from showing up as an interface

--- vlans.py
+++ vlans.py
@@ -5,9 +5,10 @@ def probe():
 	ifaces = []
 	pairs = []
 	for vlan in os.listdir('/proc/net/vlan'):
-		ifaces.append(vlan)
 		if vlan == 'config':
 			continue
+
+		ifaces.append(vlan)
 		for line in open('/proc/net/vlan/%s' % vlan):
 			if line.startswith('Device:'):
 				pairs.append((vlan, line.split()[1]))
