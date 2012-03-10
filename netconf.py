4ca4c640c483ad64229ff6bf4760e84809bf2668
Lars Kellogg-Stedman <lars@oddbit.com>
2012-03-09 23:31:26 -0500

    added per-interface-type attributes

--- netconf.py
+++ netconf.py
@@ -13,21 +13,37 @@ import bridges
 import vlans
 import physdevs
 
+attrs = {
+        'physdevs'  : {'color':'firebrick1'},
+        'bonds'     : {'color':'darkgoldenrod1'},
+        'bridges'   : {'color':'cornflowerblue'},
+        'vlans'     : {'color':'darkolivegreen2'},
+        }
+
 def discover():
+    ifaces = {}
     pairs = []
 
     for x in [ physdevs, bonds, bridges, vlans ]:
-        pairs.extend(x.probe()[1])
+        res = x.probe()
 
-    return pairs
+        ifaces[x.__name__] = res[0]
+        pairs.extend(res[1])
 
-def render(pairs):
+    return (ifaces, pairs)
+
+def render(ifaces, pairs):
     print 'digraph net {'
-    print 'graph [rank=LR]'
+    print 'graph [rankdir=LR]'
     print
+    for k,v in ifaces.items():
+        nodeattr = ' '.join(['%s=%s' % (n,v) for n,v in attrs[k].items()])
+        for iface in v:
+            print '"%s" [%s]' % (iface, nodeattr)
+
     for a,b in pairs:
         print '"%s" -> "%s"' % (a,b)
     print '}'
 
-render(discover())
+render(*discover())
 
