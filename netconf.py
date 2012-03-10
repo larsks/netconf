cddf55b4030d68452fa42a761e5bb036c61e1076
Lars Kellogg-Stedman <lars@oddbit.com>
2012-03-10 16:07:22 -0500

    make graph prettier

--- netconf.py
+++ netconf.py
@@ -4,49 +4,110 @@ import os
 import sys
 import threading
 
-from scapy.all import *
-
 import lldp
 
 import bonds
 import bridges
 import vlans
-import physdevs
+#import physdevs
 
 attrs = {
-        'physdevs'  : {'color':'firebrick1', 'style':'filled'},
-        'bonds'     : {'color':'darkgoldenrod1', 'style':'filled'},
-        'bridges'   : {'color':'cornflowerblue', 'style':'filled'},
-        'vlans'     : {'color':'darkolivegreen2', 'style':'filled'},
+        '__default__': {
+            'subgraph': {
+                'rank' : 'same',
+                },
+            'node': {
+                'style' : 'filled',
+                },
+            },
+        'ports': {
+            'node': {
+                'shape' : 'box',
+                },
+            },
+        'physdevs': {
+            'node': {
+                'color' : 'firebrick1',
+                },
+            },
+        'bonds': {
+            'node': {
+                'color' : 'darkgoldenrod1',
+                },
+            },
+        'bridges': {
+            'subgraph': {
+                'rank' : 'min',
+                },
+            'node': {
+                'color' : 'cornflowerblue',
+                },
+            },
+        'vlans': {
+            'node': {
+                'color' : 'darkolivegreen2',
+                },
+            },
         }
 
+
 def discover():
-    ifaces = {}
-    pairs = []
+    data = {}
 
     for x in [ physdevs, bonds, bridges, vlans ]:
-        res = x.probe()
+        ifaces, pairs = x.probe()
 
-        ifaces[x.__name__] = res[0]
-        pairs.extend(res[1])
+        data[x.__name__] = (ifaces, pairs)
+        if x.__name__ == 'physdevs':
+            data['ports'] = ([x[1] for x in pairs], [])
 
-    return (ifaces, pairs)
+    return data
 
-def render(ifaces, pairs):
+def render(data):
     print 'digraph net {'
-    print 'graph [rankdir=LR]'
+    print '  rankdir=LR;'
     print
-    for iftype,ifaces in ifaces.items():
-        print 'subgraph {'
-        print 'rank = same'
-        nodeattr = ' '.join(['%s=%s' % (n,v) for n,v in attrs[iftype].items()])
-        for iface in ifaces:
-            print '"%s" [%s]' % (iface, nodeattr)
-        print '}'
 
-    for a,b in pairs:
-        print '"%s" -> "%s"' % (a,b)
+    for iftype in [ 'ports', 'physdevs', 'bonds', 'bridges', 'vlans' ]:
+
+        subgraph_attrs = {}
+        subgraph_attrs.update(attrs['__default__']['subgraph'])
+        subgraph_attrs.update(attrs[iftype].get('subgraph', {}))
+
+        node_attrs = {}
+        node_attrs.update(attrs['__default__']['node'])
+        node_attrs.update(attrs[iftype].get('node', {}))
+
+        print
+        print '  /* %s */' % iftype
+        print '  subgraph {'
+
+        for k,v in subgraph_attrs.items():
+            print '    %s=%s ;' % (k,v)
+
+        print '    node [%s] ;' % (', '.join(
+            ['%s=%s' % (n,v) for n,v in node_attrs.items()]))
+        print
+        for iface in data[iftype][0]:
+            print '    "%s" ;' % iface
+
+        print '  }'
+
+    print
+    for iftype in [ 'ports', 'physdevs', 'bonds', 'bridges', 'vlans' ]:
+        for k,v in data[iftype][1]:
+            print '  "%s" -> "%s" ;' % (k,v)
+
     print '}'
 
-render(*discover())
-
+if __name__ == '__main__':
+    render(discover())
+#    render({
+#        'ports': [['eth104/1/48', 'eth104/1/46'],[]],
+#        'physdevs': [['em1', 'em2'],[('em1', 'eth104/1/48'), ('em2',
+#            'eth104/1/46')]],
+#        'bonds': [[],[]],
+#        'bridges': [[],[]],
+#        'vlans': [[],[]],
+#        })
+#
