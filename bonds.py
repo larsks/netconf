168948594b40076816d26306300c483459951d69
Lars Kellogg-Stedman <lars@oddbit.com>
2012-03-10 20:30:51 -0500

    fail gracefully if there are no bonding interfaces

--- bonds.py
+++ bonds.py
@@ -1,13 +1,18 @@
 import os
 import glob
+import errno
 
 def probe():
 	ifaces = []
 	pairs = []
-	for iface in open('/sys/class/net/bonding_masters').read().split():
-		ifaces.append(iface)
-		for line in open('/sys/class/net/%s/bonding/slaves' % iface).read().split():
-			pairs.append((iface, line.strip()))
+    try:
+        for iface in open('/sys/class/net/bonding_masters').read().split():
+            ifaces.append(iface)
+            for line in open('/sys/class/net/%s/bonding/slaves' % iface).read().split():
+                pairs.append((iface, line.strip()))
+    except IOError, detail:
+        if detail.errno != errno.ENOENT:
+            raise
 
 	return (ifaces, pairs)
 
